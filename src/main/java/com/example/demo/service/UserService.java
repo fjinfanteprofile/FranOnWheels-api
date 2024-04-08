5ea1d1b9177ab1cb61dc781196b298cc0ba9e4de
fjinfanteprofile <fjinfante@profile.es>
2024-04-08 16:15:58 +0200

    added the bulk insertion when initializing, corrected entities, added extra @query in the UserRepository.java to find only admin users

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,7 +3,6 @@ package com.example.demo.service;
 import com.example.demo.model.domain.User;
 import com.example.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
