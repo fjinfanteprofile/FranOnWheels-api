17ef3cab984058ed1f5aceeb5aa351e55fc410bd
fjinfanteprofile <fjinfante@profile.es>
2024-04-22 11:21:43 +0200

    WIP BookingsServiceUnitTest.java

--- src/main/java/com/example/franonwheels/controller/UserController.java
+++ src/main/java/com/example/franonwheels/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.example.franonwheels.controller;
 
+import com.example.franonwheels.model.dtos.UserDTO;
+import com.example.franonwheels.service.impl.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserServiceImpl userServiceImpl;
+
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
+        UserDTO createdUser = userServiceImpl.createUser(userDTO);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+
+
 }
